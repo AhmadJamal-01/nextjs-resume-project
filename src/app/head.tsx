e6c8e962166957bad2b5cfe1cab84903a1d550b1
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-04-20 02:21:09 +0500

    Create Next JS

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Head () {
+	return (
+		<>
+			<title>Resume</title>
+			<meta content='width=device-width, initial-scale=1' name='viewport' />
+			<link rel='icon' href='./app/favicon.ico' />
+		</>
+	);
+}
+
+export default Head;
