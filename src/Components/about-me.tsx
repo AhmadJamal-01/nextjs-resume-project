53a1309b1617276406c518064c931c7933961fa1
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-04-21 22:58:51 +0500

    Update Any Type

--- src/Components/about-me.tsx
+++ src/Components/about-me.tsx
@@ -1,13 +1,13 @@
 import React, {useId} from "react";
 import Skills from "./skills"
 
-const AboutMe = ({data, skills}) => {
+const AboutMe = ({data, skills}:any) => {
 const { title, body} = data;
 const id = useId();
     return(
 <section>
     <h2 className="mb-8">{title}</h2>
-   {body?.map((el, i) => (<p key={`${id}_${i} `} className="mb-6"> {el} </p>))}
+   {body?.map((el:any, i:any) => (<p key={`${id}_${i} `} className="mb-6"> {el} </p>))}
         
 <Skills data={skills}/>
 </section>
